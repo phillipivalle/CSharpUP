6526d7782a240c320ee31e95c14d253d156ad1d1
phillipivalle <41653782+phillipivalle@users.noreply.github.com>
2018-07-31 21:26:35 -0300

    Interações com usuário

--- AulasUP/Aula3107_Console/Program.cs
+++ AulasUP/Aula3107_Console/Program.cs
@@ -8,8 +8,17 @@ namespace Aula3107_Console
 {
     class Program
     {
+        public static int Id { get; set; }
         static void Main(string[] args)
         {
+            Console.WriteLine("Meu mouse é uma bosta");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Informe um número:");
+            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("você digitou" + Id);
+            Console.ReadKey();
         }
     }
 }

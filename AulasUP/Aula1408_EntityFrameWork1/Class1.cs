ccaa82cd92fe7f497c2d13ccc80da1653461e70c
phillipivalle <41653782+phillipivalle@users.noreply.github.com>
2018-08-14 21:40:13 -0300

    Instalando Entity FrameWork

---
+++ AulasUP/Aula1408_EntityFrameWork1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1408_EntityFrameWork1
+{
+
+    /*
+     ##### Conceitos
+
+
+
+
+        ?
+
+
+
+
+
+
+    ##### Instalando Entity FrameWork
+
+
+
+
+    */
+
+
+    public class Class1
+    {
+    }
+}

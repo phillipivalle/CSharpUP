0ff12bde25db71612bb844326a9403f682d3422f
phillipivalle <41653782+phillipivalle@users.noreply.github.com>
2018-08-16 21:56:24 -0300

    Finalizaçao da aula

--- AulasUP/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
+++ AulasUP/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -8,5 +8,39 @@ namespace Aula1608_EFDatabaseFirst.Controllers
 {
     class PersonController
     {
+
+        void inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+        List<Person> ListarTodosPerson()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+        }
+
+        Person BuscarPorID(int ID)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+            return contexto.Person.Find(ID);
+        }
+
+        void Excluir (int ID)
+        {
+
+            Person pExcluir = BuscarPorID(ID);
+
+            if(pExcluir != null)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+
+        }
     }
 }

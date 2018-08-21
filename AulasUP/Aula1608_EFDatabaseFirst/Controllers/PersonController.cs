a2b61fbf8353695511d5016c8401bd7dd92f6014
phillipivalle <41653782+phillipivalle@users.noreply.github.com>
2018-08-21 20:01:38 -0300

    Finalização Controller

--- AulasUP/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
+++ AulasUP/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -12,7 +12,6 @@ namespace Aula1608_EFDatabaseFirst.Controllers
         void inserir(Person p)
         {
             AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
-
             contexto.Person.Add(p);
             contexto.SaveChanges();
         }
@@ -25,22 +24,50 @@ namespace Aula1608_EFDatabaseFirst.Controllers
         Person BuscarPorID(int ID)
         {
             AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
-
             return contexto.Person.Find(ID);
         }
 
-        void Excluir (int ID)
+        void Excluir(int ID)
         {
-
             Person pExcluir = BuscarPorID(ID);
 
-            if(pExcluir != null)
+            if (pExcluir != null)
             {
                 AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
                 contexto.Person.Remove(pExcluir);
                 contexto.SaveChanges();
             }
-
         }
+
+            void Editar(int id, Person novosDadosPerson)
+            {
+                Person personAntigo = BuscarPorID(id);
+                if(personAntigo != null)
+                {
+                    personAntigo.FirstName = novosDadosPerson.FirstName;
+                    personAntigo.LastName = novosDadosPerson.LastName;
+                    personAntigo.Title = novosDadosPerson.Title;
+
+                    AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+                    contexto.Entry(personAntigo).State = System.Data.Entity.EntityState.Modified;
+                    contexto.SaveChanges();
+
+                }
+            }
+
+            List<Person> PesquisaPorFirstName(String firstName)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            //  var lista = from p in contexto.Person
+            //            select p;
+            var lista = from p in contexto.Person
+                        where p.FirstName == firstName
+                        select p;
+
+            return lista.ToList();
+        }
+
+        
     }
 }
